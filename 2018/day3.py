89189adfcf2edca737afcd9194b2c7ac3927d996
Mat Sharff <msharff@xevo.com>
2018-12-03 01:57:52 -0800

    Added day 3 solutions

---
+++ 2018/day3.py
@@ -0,0 +1,46 @@
+import re
+
+from collections import defaultdict
+
+import advent_of_code
+
+
+def get_claimed_fabric(claims):
+    fabric = defaultdict(int)
+    for claim in claims:
+        for x in range(int(claim['left_edge']), int(claim['left_edge']) + int(claim['width'])):
+            for y in range(int(claim['top_edge']), int(claim['top_edge']) + int(claim['height'])):
+                fabric[(x, y)] += 1
+    return fabric
+
+
+def get_overlapping_area_of_claims(fabric):
+    return sum(1 for coord in fabric.values() if coord > 1)
+
+
+def get_claims():
+    day3_input = advent_of_code.get_day_input(3)
+    r = re.compile('#(?P<ID>\d+) @ (?P<left_edge>\d+),(?P<top_edge>\d+): (?P<width>\d+)x(?P<height>\d+)')
+    return [m.groupdict() for m in r.finditer(day3_input)]
+
+
+def claim_overlaps(claim, fabric):
+    for x in range(int(claim['left_edge']), int(claim['left_edge']) + int(claim['width'])):
+        for y in range(int(claim['top_edge']), int(claim['top_edge']) + int(claim['height'])):
+            if fabric[(x, y)] > 1:
+                return True
+    return False
+
+
+def get_perfect_claim_id(claims, fabric):
+    for claim in claims:
+        if not claim_overlaps(claim, fabric):
+            return claim['ID']
+
+
+if __name__ == '__main__':
+    day3_claims = get_claims()
+    claimed_fabric = get_claimed_fabric(day3_claims)
+
+    print(get_overlapping_area_of_claims(claimed_fabric))
+    print(get_perfect_claim_id(day3_claims, claimed_fabric))
