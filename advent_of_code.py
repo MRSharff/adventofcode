c9e1cca4a703a87eec0f387abe25eeb59d6cb47c
Mat Sharff <msharff@xevo.com>
2018-12-02 22:54:48 -0800

    Added rudimentary input caching

--- advent_of_code.py
+++ advent_of_code.py
@@ -9,8 +9,30 @@ class AdventOfCodeException(Exception):
     pass
 
 
-def get_day_input(day):
+def get_cached_input(input_file_path):
+    with open(input_file_path) as advent_input_file:
+        advent_input = advent_input_file.read()
+        return advent_input
+
+
+def write_response(input_path, response):
+    os.makedirs(os.path.dirname(input_path))
+    with open(input_path, 'w') as advent_input:
+        advent_input.write(response.text)
+
+
+def fetch_day_input(day):
     response = requests.get('https://adventofcode.com/2018/day/{}/input'.format(day), cookies={'session': session})
     if not response.ok:
         raise AdventOfCodeException('Could not get day input: bad response code {}'.format(response.status_code))
-    return response.text
+    return response
+
+
+def get_day_input(day):
+    input_file_path = 'inputs/day{}.txt'.format(day)
+    try:
+        return get_cached_input(input_file_path)
+    except FileNotFoundError:
+        response = fetch_day_input(day)
+        write_response(input_file_path, response)
+        return response.text
