d782ddc77260d3c92b0f57e965d8cda5973e101a
Mat Sharff <msharff@xevo.com>
2018-12-04 22:29:18 -0800

    Removed shift logic from day4 as it was not needed

--- 2018/day4.py
+++ 2018/day4.py
@@ -18,34 +18,28 @@ def make_shift(log_line):
 
 
 def make_guard(guard_number):
-    return {'ID': guard_number, 'shifts': []}
+    return {'ID': guard_number, 'sleep_times': []}
 
 
-def get_guards_and_shifts(schedule_log):
+def get_guards(schedule_log):
     guards = {}
-    shifts = []
-    current_shift = None
+    current_guard = None
     sleep_start = None
     for log_line in schedule_log:
         if "Guard" in log_line:
-            if current_shift is not None:
-                shifts.append(current_shift)
             guard_number = get_guard_number(log_line)
             if guard_number not in guards:
                 guards[guard_number] = make_guard(guard_number)
-            current_shift = make_shift(log_line)
-            current_shift['guard'] = guards[guard_number]
-            current_shift['guard']['shifts'].append(current_shift)
+            current_guard = guards[guard_number]
         if 'sleep' in log_line:
             sleep_start = parse_date(log_line).minute
         if 'wake' in log_line:
-            current_shift['asleep'].append(range(sleep_start, parse_date(log_line).minute))
-    shifts.append(current_shift)
-    return guards, shifts
+            current_guard['sleep_times'].append(range(sleep_start, parse_date(log_line).minute))
+    return guards
 
 
 def get_total_sleep(guard):
-    return sum(len(sleep) for shift in guard['shifts'] for sleep in shift['asleep'])
+    return sum(len(sleep) for sleep in guard['sleep_times'])
 
 
 def get_sleepiest_guard(guards):
@@ -54,10 +48,9 @@ def get_sleepiest_guard(guards):
 
 def get_minutes_slept(guard):
     minutes_slept = defaultdict(int)
-    for shift in guard['shifts']:
-        for sleep in shift['asleep']:
-            for minute in sleep:
-                minutes_slept[minute] += 1
+    for sleep in guard['sleep_times']:
+        for minute in sleep:
+            minutes_slept[minute] += 1
     return minutes_slept
 
 
@@ -69,13 +62,13 @@ def get_sleepiest_minute(guard):
 
 def get_guard_most_frequently_asleep_on_same_minute(guards):
     sleepiest_minute_counts = {ID: get_sleepiest_minute(guard)[1] for ID, guard in guards.items() if
-                               any(len(shift['asleep']) > 0 for shift in guard['shifts'])}
+                               len(guard['sleep_times']) > 0}
     return guards[max(sleepiest_minute_counts, key=sleepiest_minute_counts.get)]
 
 
 def part1(day4_input):
     schedule_log = sorted(day4_input.splitlines())
-    guards, shifts = get_guards_and_shifts(schedule_log)
+    guards = get_guards(schedule_log)
     sleepiest_guard = get_sleepiest_guard(guards)
     minute_asleep_most, count = get_sleepiest_minute(sleepiest_guard)
     return sleepiest_guard['ID'] * minute_asleep_most
@@ -83,7 +76,7 @@ def part1(day4_input):
 
 def part2(day4_input):
     schedule_log = sorted(day4_input.splitlines())
-    guards, shifts = get_guards_and_shifts(schedule_log)
+    guards = get_guards(schedule_log)
     guard_most_frequently_asleep_on_same_minute = get_guard_most_frequently_asleep_on_same_minute(guards)
     return (guard_most_frequently_asleep_on_same_minute['ID'] *
             get_sleepiest_minute(guard_most_frequently_asleep_on_same_minute)[0])
