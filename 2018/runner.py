74fe5c19fd311e8bbb7104ca6f555a8d593c90c1
Mat Sharff <msharff@xevo.com>
2018-12-04 22:02:32 -0800

    Cleane up runner

--- 2018/runner.py
+++ 2018/runner.py
@@ -10,10 +10,18 @@ days = [
 ]
 
 
+def print_solutions_for_day(day, solver):
+    print('Day {}:'.format(day))
+    dayinput = advent_of_code.get_day_input(day)
+    print(solver.part1(dayinput))
+    print(solver.part2(dayinput))
+    print()
+
+
+def main():
+    for day, solver in enumerate(days, start=1):
+        print_solutions_for_day(day, solver)
+
+
 if __name__ == '__main__':
-    for day, solution in enumerate(days, start=1):
-        print('Day {}:'.format(day))
-        dayinput = advent_of_code.get_day_input(day)
-        print(solution.part1(dayinput))
-        print(solution.part2(dayinput))
-        print()
+    main()
