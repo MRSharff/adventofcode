1a6f5877856bbd9ea9cc8b9bfe131240cf9f171a
Mathew Sharff <mrsharff@gmail.com>
2018-12-21 19:09:29 -0800

    Automated runner dayh imports

--- 2018/runner.py
+++ 2018/runner.py
@@ -1,26 +1,15 @@
 import advent_of_code
-import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11
+import importlib
+import glob
 
 
-days = [
-    day1,
-    day2,
-    day3,
-    day4,
-    day5,
-    day6,
-    day7,
-    day8,
-    day9,
-    day10,
-    day11
-]
+days = {day[:-3]: importlib.import_module(day[:-3]) for day in glob.glob('day*.py')}
 
 
 def print_solutions_for_day(day):
     print('Day {}:'.format(day))
     dayinput = advent_of_code.get_day_input(day)
-    solver = days[day - 1]
+    solver = days['day{}'.format(day)]
     for part in range(1, 3):
         part_solver = 'part{}'.format(part)
         if hasattr(solver, part_solver):
